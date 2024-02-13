7c5f392905753e82dd302647ce12e7ab1884f167
Vedanshi555 <umesha.ap71@gmail.com>
2024-02-13 21:02:46 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Offers from "./components/Offers";
 import Error from "./components/Error";
 import Help from "./components/Help";
 import ResMenu from "./components/ResMenu";
